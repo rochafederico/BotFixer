fc94f6458bc8f95cc5f8287b8b823ddc7feb1748
Federico Rocha <federiconrocha@gmail.com>
2025-04-01 23:47:13 -0300

    feat: initial API setup with basic endpoints

---
+++ src/main/java/com/rochafederico/botfixer/BotFixerApplication.java
@@ -0,0 +1,11 @@
+package com.rochafederico.botfixer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotFixerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BotFixerApplication.class, args);
+    }
+}
